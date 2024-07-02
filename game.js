b2bf008001aff1a352c5de5814b11e1160061dbc
Sean <sean.devaney2002@gmail.com>
2024-07-01 22:55:27 -0500

    added a reset button and a score for the players. Also added css so that background color changes per player

--- game.js
+++ game.js
@@ -2,13 +2,6 @@ import { checkWinner } from "./checkWinner.js";
 let symbol = "X"
 
 
-function squareX(element) {
-    element.textContent = symbol;
-}
-
-function squareO(element) {
-    element.textContent = symbol;
-}
 const Player = (char) => {
     let symbol = char;
     const getSymbol = () => symbol;
@@ -25,11 +18,6 @@ const Player = (char) => {
     const getChoice = () => choice;
     const setChoice = (symbol) => {
         choice = symbol;
-        /*let num = prompt("enter square to put character");
-        console.log(num);
-        choice = num;
-        console.log("test");
-        */
         return;
     }
 
@@ -52,6 +40,7 @@ const gameBoard = (() => {
         for(let i = 0; i < 9; i++) {
             //console.log(board[0]);
             const square = document.createElement("div");
+            
             if (board[i] !== '') {
                 square.textContent = board[i];
             }
@@ -87,24 +76,20 @@ const gameBoard = (() => {
     return{getBoard, setBoard, getGameOver, setGameOver,render, clearBoard};
 })();
 
-const gameController = (() => {
-
-})();
 
 let clickHandler = function(square) {
-    console.log(Number(square));
-    console.log(symbol);
     let id = square;
     let empty;
     if (symbol === "X") {
         if(gameBoard.getBoard()[Number(id)] === "") {
             empty = true;
             playerOne.setChoice(Number(id));
-            console.log(playerOne.getChoice());
             gameBoard.setBoard("X",playerOne.getChoice());
-            console.log(gameBoard.getBoard());
             gameBoard.render(gameBoard.getBoard());
             symbol = "O";
+            document.querySelector(".body").style.backgroundColor = "#fc8686";
+            document.querySelector(".scoreOne").style.fontWeight = "normal";
+            document.querySelector(".scoreTwo").style.fontWeight = "bold";
         } else {
             empty = false
         }
@@ -112,11 +97,12 @@ let clickHandler = function(square) {
         if(gameBoard.getBoard()[Number(id)] === "") {
             empty = true;
             playerTwo.setChoice(Number(id));
-            console.log(playerTwo.getChoice());
             gameBoard.setBoard("O",playerTwo.getChoice());
-            console.log(gameBoard.getBoard());
             gameBoard.render(gameBoard.getBoard());
             symbol = "X";
+            document.querySelector(".body").style.backgroundColor = "#86dffc";
+            document.querySelector(".scoreOne").style.fontWeight = "bold";
+            document.querySelector(".scoreTwo").style.fontWeight = "normal";
         } else {
             empty = false
         }
@@ -124,75 +110,56 @@ let clickHandler = function(square) {
     }
     if (empty) {
         let result = checkWinner(gameBoard.getBoard());
-        console.log(result);
         if (result != "") {
             if (result === "X") {
                 playerOne.setGamesWon(playerOne.getGamesWon() + 1);
-                console.log(gameBoard.getBoard());
                 gameBoard.clearBoard();
-                console.log(gameBoard.getBoard());
-                gameBoard.render(gameBoard.getBoard);
+                gameBoard.render(gameBoard.getBoard());
+                const score = document.querySelector(".scoreOne");
+                console.log(playerOne.getGamesWon());
+                score.textContent = "Player 1 - " + String(playerOne.getGamesWon());
+                symbol = "X";
+                document.querySelector(".body").style.backgroundColor = "#86dffc";
+                document.querySelector(".scoreOne").style.fontWeight = "bold";
+                document.querySelector(".scoreTwo").style.fontWeight = "normal";
             } else if (result === "O") {
                 playerTwo.setGamesWon(playerTwo.getGamesWon() + 1);
-                console.log(gameBoard.getBoard());
                 gameBoard.clearBoard();
-                console.log(gameBoard.getBoard());
-                gameBoard.render(gameBoard.getBoard);
+                gameBoard.render(gameBoard.getBoard());
+                const score = document.querySelector(".scoreTwo");
+                score.textContent = "Player 2 - " + String(playerOne.getGamesWon());
+                symbol = "O";
+                document.querySelector(".body").style.backgroundColor = "#fc8686";
+                document.querySelector(".scoreOne").style.fontWeight = "normal";
+                document.querySelector(".scoreTwo").style.fontWeight = "bold";
             } else {
-                console.log(gameBoard.getBoard());
                 gameBoard.clearBoard();
-                console.log(gameBoard.setBoard());
-                gameBoard.render(gameBoard.getBoard);
-                //tie
-
+                gameBoard.render(gameBoard.getBoard());
+                symbol = "X";
+                document.querySelector(".body").style.backgroundColor = "#86dffc";
+                document.querySelector(".scoreOne").style.fontWeight = "bold";
+                document.querySelector(".scoreTwo").style.fontWeight = "normal";
             }
     }
-    //if (square.textContent == "") {
-    //    square.textContent = "X"
-    //}
-    //console.log(Number(square.className) - 1);
     console.log("done");
     return;
     }
 }
 const playerOne = Player("X");
 const playerTwo = Player("O");
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+    gameBoard.clearBoard()
+    gameBoard.render(gameBoard.getBoard());
+    const scoreOne = document.querySelector(".scoreOne");
+    const scoreTwo = document.querySelector(".scoreTwo");
+    console.log(scoreOne.textContent);
+    scoreOne.textContent = "Player 1 - 0";
+    scoreTwo.textContent = "Player 2 - 0";
+    symbol = "X"
+    document.querySelector(".body").style.backgroundColor = "#86dffc";
+    document.querySelector(".scoreOne").style.fontWeight = "bold";
+    document.querySelector(".scoreTwo").style.fontWeight = "normal";
+})
 
 gameBoard.render(gameBoard.getBoard());
-/*const squares = document.querySelectorAll(".game-board div");
-console.log(squares.length);
-for(let square of squares) {
-    square.addEventListener("click", function() {
-        clickHandler(square.id);
-    });
-}
-
-*/
-//gameBoard.render(gameBoard.getBoard());
-//console.log("1");
-//playerOne.setSymbol('X');
-//console.log(gameBoard.getGameOver());
-/*while(!gameBoard.getGameOver()) {
-    playerOne.setChoice()
-    let choice = playerOne.getChoice();
-    gameBoard.setBoard(playerOne.getSymbol(), parseInt(choice));
-    gameBoard.render(gameBoard.getBoard());
-    console.log(gameBoard.getBoard());
-    let result = checkWinner(gameBoard.getBoard());
-    if (result !== '') {
-        console.log(result);
-        gameBoard.setGameOver();
-        
-    }
-    playerTwo.setChoice()
-    choice = playerTwo.getChoice();
-    gameBoard.setBoard(playerTwo.getSymbol(), parseInt(choice));
-    gameBoard.render(gameBoard.getBoard());
-    console.log(gameBoard.getBoard());
-    result = checkWinner(gameBoard.getBoard());
-    if (result !== '') {
-        console.log(result);
-        gameBoard.setGameOver();        
-    }
-}
-*/
